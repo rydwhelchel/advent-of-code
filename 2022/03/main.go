12b3dfc995b457c32d560175c5a574ee4ac3be5b
Ryan Whelchel <rydwhelchel@gmail.com>
2024-03-15 21:31:22 -0400

    Golang day 3

--- 2022/03/main.go
+++ 2022/03/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -22,9 +23,59 @@ func main() {
 
     part1 := part1(lines)
     log.Printf("Part1: %v\n", part1)
+    part2 := part2(lines)
+    log.Printf("Part2: %v\n", part2)
 }
 
 func part1(lines []string) int {
+    sum := 0
 
-    return 0
+    for _, line := range lines {
+        midmark := len(line)/2
+        firstHalf := line[0:midmark]
+        secondHalf := line[midmark:]
+        for _, char := range firstHalf {
+            if strings.Contains(secondHalf, string(char)) {
+                sum += calcValue(char)
+                break
+            } 
+        }
+
+    }
+
+    return sum
+}
+
+func part2(lines []string) int {
+    sum := 0
+
+    group := []string{}
+    for _, line := range lines {
+        if len(group) < 3 {
+            group = append(group, line)
+            if len(group) < 3 {
+                continue
+            }
+        }
+        firstLine := group[0]
+        secondLine := group[1]
+        thirdLine := group[2]
+        for _, char := range firstLine {
+            if strings.Contains(secondLine, string(char)) && strings.Contains(thirdLine, string(char)) {
+                sum += calcValue(char)
+                break
+            }
+        }
+        group = []string{}
+    }
+
+    return sum
+}
+
+
+func calcValue(r rune) int {
+    if r > 96 {
+        return int(r) - 96
+    }
+    return int(r) - 64 + 26
 }
